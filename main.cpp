2eb868d41a49cde4f366843717de77c9a3367de8
daiki Iijima <daiki.applecreate@gmail.com>
2024-02-14 01:38:16 +0900

    feat : 使用するOpenGLのバージョンとダブルバッファのスワップタイミングの設定を追加

--- main.cpp
+++ main.cpp
@@ -13,12 +13,23 @@ int main() {
     //  終了時の処理で必ず呼び出されるようにTerminate関数を登録
     atexit(glfwTerminate);
 
+    //  使用するOpenGLのバージョンを指定
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,6);
+    //  OpenGL3.0以降の機能のみを使用するための設定
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
     //  ウィンドウ生成
     GLFWwindow* window = glfwCreateWindow(800,640,"OpeGLサンプル", nullptr, nullptr);
     if(window == nullptr){
         std::cerr << "ウィンドウの生成に失敗" << std::endl;
         return EXIT_FAILURE;
     }
+
+    //  垂直同期のタイミングを待つ
+    //  カラーバッファのスワップタイミングの指定
+    glfwSwapInterval(1);
 
     //  生成したウィンドウのコンテキストをカレントにする
     glfwMakeContextCurrent(window);
