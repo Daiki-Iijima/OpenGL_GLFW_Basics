55f852ba5621ad7949d523b695984f5ab0d83582
daiki Iijima <daiki.applecreate@gmail.com>
2024-02-14 01:25:47 +0900

    feat : add output opengl version function

--- main.cpp
+++ main.cpp
@@ -16,6 +16,16 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    //  OpenGLのバージョンを取得
+    const GLubyte* version = glGetString(GL_VERSION);
+    if(version != nullptr){
+        std::cout << "Version: " << version << std::endl;
+    }
+
+    //  クリアカラーの設定
+    glClearColor(1.f, 1.f, 1.f, 1.f);
+
+    //  メインループ
     while(glfwWindowShouldClose(window) == GL_FALSE){
         glfwWaitEvents();
     }
